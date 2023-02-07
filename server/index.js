5271b9fd7dc19d06da424c5777eeebf8e7bf28cf
coder-Sabarish <studysabarish@gmail.com>
2023-02-07 15:31:24 +0530

    mondoDB connected successfully

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+const app = express();
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+
+//mongoose connection
+const PORT = process.env.PORT || 6000;
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useunifiedTopology: true,
+})
+.then(() => {
+    app.listen(PORT, () => console.log(`DB connected Successfully`));
+
+    //User.insertMany(users);
+    //Post.insertMany(posts);
+})
+.catch((error) => console.log(`${error} did not connect`));
+
+console.log(`Server running at port ${PORT}`);
