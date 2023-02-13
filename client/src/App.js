2823fb2875864f5851344192e3427dc731223fd1
coder-Sabarish <90779418+coder-Sabarish@users.noreply.github.com>
2023-02-13 15:39:09 +0530

    resolving conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
-import Login from "./Pages/Login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+    
     </div>
   );
 }
