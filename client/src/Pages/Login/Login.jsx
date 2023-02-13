5618ea5288e825ba4fabe154a9173e90a08dbdfd
SaravanakKumarTA <saravanakkumarta.19cse@kongu.edu>
2023-02-13 15:21:21 +0530

    login comp

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,30 +1,49 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import "./Login.css";
 function Login() {
   return (
-    <>
-      <center>
-        <div className="form">
-          <form>
-            <h1>Login</h1>
-            <br />
-            <br />
-            <div>
-              <label>Username</label>
-              <input type="text" placeholder="Enter your username" />
-            </div>
-            <br />
-            <br />
-            <div>
-              <label>password</label>
-              <input type="password" placeholder="Type your password" />
-            </div>
-            <br />
-            <p id="forgotPassword">forgot Password?</p>
-          </form>
-        </div>
-      </center>
-    </>
+    <div className="content">
+      <div
+        className="sep"
+        style={{ backgroundColor: "white", border: "none", borderRadius: "0%" }}
+      >
+        <Form className="sep">
+          <br />
+
+          <h2 style={{ color: "blue" }}>Login</h2>
+          <br />
+          <br />
+
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="email"
+              placeholder="email address"
+              style={{ backgroundColor: "white", border: "none" }}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              style={{ backgroundColor: "white", border: "none" }}
+            />
+          </Form.Group>
+
+          <a href="http://www.google.com">forgot password</a>
+          <br />
+          <br />
+
+          <Button variant="primary" type="submit">
+            Sign in
+          </Button>
+        </Form>
+      </div>
+      <div className="half imginary" />
+    </div>
   );
 }
 export default Login;
